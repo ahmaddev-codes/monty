707580b2ca3f3433c1c77e79b5438c5ee6c8126d
iahm-codes <omegabliss23@.com>
2024-01-02 18:22:07 +0100

    Code: rearranged files

--- monty.h
+++ monty.h
@@ -1,12 +1,12 @@
-#ifndef _MONTY_
-#define _MONTY_
+#ifndef MONTY
+#define MONTY
 
 /* Constants */
-#define SUCSS_OP		0
-#define VALID_PARM		0
-#define MIN_ARGS		2
-#define METH_STACK		300
-#define METH_QUEUE		301
+#define SUCSS_OP	0
+#define VALID_PARM	0
+#define MIN_ARGS	2
+#define METH_STACK	300
+#define METH_QUEUE	301
 
 /* Common Errors */
 #define ERR_BAD_INST	100
@@ -14,19 +14,19 @@
 #define ERR_INVLD_PARM	102
 
 /* Usage Errors */
-#define ERR_ARG_USG		200
+#define ERR_ARG_USG	200
 #define ERR_PUSH_USG	201
 #define ERR_PINT_USG	202
-#define ERR_POP_USG		203
+#define ERR_POP_USG	203
 #define ERR_SWAP_USG	204
-#define ERR_ADD_USG		205
-#define ERR_SUB_USG		206
-#define ERR_DIV_USG		207
-#define ERR_DIV_ZRO		208
-#define ERR_MUL_USG		209
-#define ERR_MOD_USG		210
-#define ERR_PCH_USG		211
-#define ERR_PCH_EMP		212
+#define ERR_ADD_USG	205
+#define ERR_SUB_USG	206
+#define ERR_DIV_USG	207
+#define ERR_DIV_ZRO	208
+#define ERR_MUL_USG	209
+#define ERR_MOD_USG	210
+#define ERR_PCH_USG	211
+#define ERR_PCH_EMP	212
 
 #include <ctype.h>
 #include <fcntl.h>
@@ -42,7 +42,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -57,7 +57,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
@@ -96,4 +96,5 @@ void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
-#endif
+
+#endif /* MONTY */
